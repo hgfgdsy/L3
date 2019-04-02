e92550f7c53d59a19993058fd3571dc73170473b
tracer-nju <tracer@nju.edu.cn>
2019-04-02 18:41:23 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 47d9f2a9f107420faed2101fced9000b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  18:41:24 up  3:56,  1 user,  load average: 0.47, 0.44, 0.36

--- src/os.c
+++ src/os.c
@@ -17,8 +17,10 @@ static void os_run() {
   _intr_write(1);
 
   while (1) {
-  uintptr_t po = (uintptr_t)pmm->alloc(rand()%(1<<20)+1);
-  if(po==0x230010) {printf("can't");while(1);}
+  int my_rand = rand()%(1<<20)+1;
+  printf("my_rand = %x\n",my_rand);
+  uintptr_t po = (uintptr_t)pmm->alloc(my_rand);
+  
   printf("%x\n",po);
   pmm->free((void *)po);
   if(!po) break;
