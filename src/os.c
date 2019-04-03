05e0ac1319cafb04ab2dac49e5aa7a7dded2c31b
tracer-nju <tracer@nju.edu.cn>
2019-04-03 13:33:59 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 934712b6b5a94c0fb7d3925e7731aa50   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  13:33:59 up  1:24,  1 user,  load average: 0.78, 0.48, 0.38

--- src/os.c
+++ src/os.c
@@ -60,7 +60,7 @@ static void os_run() {
   }
   else{
 	  if(cnt == 0) pmm->free(NULL);
-	  else {break;
+	  else {
 		  for(int i=0;i<=499;i++) if(cha[i] != 0) {pmm->free((void *)cha[i]); cnt--; break;
 		  }
 	  }
