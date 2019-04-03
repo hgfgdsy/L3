ac9823a732feafe6d62a30e872efb296341d1bcc
tracer-nju <tracer@nju.edu.cn>
2019-04-03 13:34:59 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 934712b6b5a94c0fb7d3925e7731aa50   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  13:34:59 up  1:25,  1 user,  load average: 0.57, 0.49, 0.39

--- src/os.c
+++ src/os.c
@@ -21,13 +21,13 @@ static void os_run() {
 //  pmm->free(p1);
 //  pmm->free(p2);
   int cnt = 0;
-  int my_cnt = 0;
+//  int my_cnt = 0;
   uintptr_t cha[500];
   for(int i=0;i<=499;i++) cha[i] = 0;
 
   while (1) {
-  my_cnt++;
-  if(my_cnt == 500) break;
+//  my_cnt++;
+//  if(my_cnt == 500) break;
   int i;
   int fk = rand()%2+1;
   if(fk == 1){
@@ -61,7 +61,7 @@ static void os_run() {
   else{
 	  if(cnt == 0) pmm->free(NULL);
 	  else {
-		  for(int i=0;i<=499;i++) if(cha[i] != 0) {pmm->free((void *)cha[i]); cnt--; break;
+		  for(int i=0;i<=499;i++) if(cha[i] != 0) {pmm->free((void *)cha[i]); cnt--; cha[i] = 0; break;
 		  }
 	  }
   }
