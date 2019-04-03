241c48a2d087a9d20d50e7e514b9cf5c7db66278
tracer-nju <tracer@nju.edu.cn>
2019-04-03 15:46:16 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 934712b6b5a94c0fb7d3925e7731aa50   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  15:46:16 up  3:36,  1 user,  load average: 0.83, 0.58, 0.49

--- src/os.c
+++ src/os.c
@@ -25,7 +25,7 @@ static void os_run() {
 
   while (1) {
   int i;
-  int fk = rand()%1+1;
+  int fk = rand()%2+1;
   if(fk == 1){
 	  if(cnt==498) {printf("I'm full"); 
 	  for(int i=0;i<=499;i++){
