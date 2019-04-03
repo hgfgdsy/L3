c5dc21b5835031e1f0e1fb842d240258e4aa1fd1
tracer-nju <tracer@nju.edu.cn>
2019-04-03 16:07:04 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 934712b6b5a94c0fb7d3925e7731aa50   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  16:07:04 up  3:57,  1 user,  load average: 0.39, 0.45, 0.50

--- src/os.c
+++ src/os.c
@@ -19,15 +19,11 @@ static void hello() {
 static void os_run() {
   hello();
   _intr_write(1);
-  void *t = pmm->alloc(1);
-  printf("%x\n",(uintptr_t)((char *)t - 16));
-  _yield();
-  pmm->free(t);
-//  int cnt = 0;
-//  uintptr_t cha[500];
-//  for(int i=0;i<=499;i++) cha[i] = 0;
+  int cnt = 0;
+  uintptr_t cha[500];
+  for(int i=0;i<=499;i++) cha[i] = 0;
 
-  while (1) {/*
+  while (1) {
   int i;
   int fk = rand()%2+1;
   if(fk == 1){
@@ -42,7 +38,7 @@ static void os_run() {
 		  int order1 = rand()%13+12;
 		  int my_rand1 = rand()%(1<<(order1-1)) +1;
                   uintptr_t po = (uintptr_t)pmm->alloc((1<<order1)+my_rand1);
-		  if(!po) {printf("allmem is %x but po = %x\n",allmem,(1<<order1)+my_rand1);break;}
+		  if(!po)// {printf("allmem is %x but po = %x\n",allmem,(1<<order1)+my_rand1);break;}
 		  {
 			  for(int i=0;i<=499;i++){
 				  if(cha[i]!=0) pmm->free((void *)cha[i]);
@@ -62,7 +58,7 @@ static void os_run() {
 		  int order2 = rand()%11+1;
 		  int my_rand2 = rand()%(1<<(order2-1)) +1;
 		  uintptr_t pi = (uintptr_t)pmm->alloc((1<<order2)+my_rand2);
-		  if(!pi) {printf("allmem is %x but pi = %x\n",allmem,(1<<order2)+my_rand2);break;}
+		  if(!pi)// {printf("allmem is %x but pi = %x\n",allmem,(1<<order2)+my_rand2);break;}
 		  {
                           for(int i=0;i<=499;i++){
 				  if(cha[i]!=0) pmm->free((void *)cha[i]);
@@ -85,7 +81,7 @@ static void os_run() {
 		  for(int i=0;i<=499;i++) if(cha[i] != 0) {allmem-=(1<<*(int *)((char *)cha[i]-12));pmm->free((void *)cha[i]); cnt--;  cha[i] = 0; break;
 		  }
 	  }
-  }*/
+  }
     _yield();
   }
 }
