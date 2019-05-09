25409c51f5d37367df094c8c2e542fc7c1bb96d5
tracer-nju <tracer@nju.edu.cn>
2019-05-09 20:43:30 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 512c894fd3d44a2bb8b60e5b9831317c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:43:30 up 43 min,  1 user,  load average: 0.16, 0.33, 0.36

--- src/kmt.c
+++ src/kmt.c
@@ -88,6 +88,7 @@ static void kmt_init(){
 static int kmt_create(task_t *task, const char *name, 
 		void (*entry)(void *arg), void *arg){
 	int rec=0;
+	printf("create 1\n");
 	for(int i = 0 ;i < 20 ;i++) {
 		if(tagging[i] == -1) {
 			rec = i;
