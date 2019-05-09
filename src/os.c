8bfde1db6dd5f0075fe66daaa9d37f8ba4a5e9b3
tracer-nju <tracer@nju.edu.cn>
2019-05-09 20:22:29 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 512c894fd3d44a2bb8b60e5b9831317c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:22:29 up 22 min,  1 user,  load average: 0.26, 0.30, 0.35

--- src/os.c
+++ src/os.c
@@ -112,6 +112,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
   kmt->spin_lock((spinlock_t *)&OT);
   _Context *ret = NULL;
   handle *now = handle_head;
+  if(now==NULL) printf("now is NULL\n");
   while(now != NULL){
 	  if(now->event == _EVENT_NULL || now->event == ev.event) {
 		  _Context *next = now->handler(ev,context);
