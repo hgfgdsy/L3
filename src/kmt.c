6894ee5fad9a0e99e2c1fb6881474ad5a5353559
tracer-nju <tracer@nju.edu.cn>
2019-05-11 10:00:26 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 32fb3b57533649e69c4a760ee761841b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  10:00:26 up 51 min,  1 user,  load average: 0.29, 0.35, 0.39

--- src/kmt.c
+++ src/kmt.c
@@ -39,9 +39,12 @@ int holding(spinlock_t *lk) {
 
 task_t *tasks[20];
 int tagging[20];
+int runable[20];
+
 
 _Context init_tasks[8];
 int osruntk[8];
+int cpuisin[8];
 
 spinlock_t CT;
 
@@ -113,6 +116,7 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
 	if(cur_rec != -1 && (_Context *)&tasks[current[_cpu()]->tag]->context != sret) panic("beforeret\n");
 	return sret;
 */
+/*
 	int cur_rec = -1;
 	if(osruntk[_cpu()] == 0) {
 		for(int i = 0; i < 20; i++) {
@@ -167,6 +171,67 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
 	}
 	if(cur_rec != -1 && (_Context *)&tasks[current[_cpu()]->tag]->context != sret) panic("beforeret\n");
 	return sret;
+*/
+	int cur_rec = -1;
+	if(osruntk[_cpu()] == 0) {
+		for(int i = 0; i < 20; i++) {
+			if(tagging[i] != -1 && tasks[i] -> incpu == _cpu() && runable[i]) {
+				cur_rec = i;
+				break;
+			}
+		}
+	}
+	else {
+		if(current[_cpu()] != tasks[current[_cpu()]->tag])
+			panic("DIFFINT\n");
+	for(int i = current[_cpu()] -> tag+1; i < 20; i++) {
+		if(tagging[i] != -1 && tasks[i] -> incpu == _cpu() && runable[i]) {
+			cur_rec = i;
+			break;
+		}
+	}
+	if(cur_rec == -1) {
+		for(int i = 0 ;i <= current[_cpu()]->tag-1; i++) {
+			if(tagging[i] != -1 && tasks[i] -> incpu == _cpu() && runable[i]) {
+				cur_rec = i;
+				break;
+			}
+		}
+	}}
+	_Context *sret;
+	if(cur_rec == -1) {
+		if(osruntk[_cpu()] == 0) {
+			sret = (_Context *)&init_tasks[_cpu()];
+		}
+		else {
+			if(runable[current[_cpu()]->tag])
+			sret = (_Context *)&(current[_cpu()] -> context);
+			else {
+				osruntk[_cpu()] = 0;
+				cpuisin[_cpu()] = -1;
+				sret = (_Context *)&init_tasks[_cpu()];
+			}
+		}
+	}
+	else {
+		if(osruntk[_cpu()] == 0) {
+			osruntk[_cpu()] = 1;
+			cpuisin[_cpu()] = cur_rec;
+//			tasks[cur_rec] -> incpu = _cpu();
+			current[_cpu()] = tasks[cur_rec];
+			sret = (_Context *)&(tasks[cur_rec] -> context);
+		}
+		else {
+//	                tasks[current[_cpu()] -> tag] -> incpu = _cpu();
+//	                tasks[cur_rec] -> incpu = _cpu();
+	                cpuisin[_cpu()] = cur_rec;
+			current[_cpu()] = tasks[cur_rec];
+	                sret = (_Context *)&(current[_cpu()] -> context);
+
+		}
+	}
+	if(cur_rec != -1 && (_Context *)&tasks[current[_cpu()]->tag]->context != sret) panic("beforeret\n");
+	return sret;
 
 /*	int cur_rec = -1;
 	if(osruntk[_cpu()] == 0) {
@@ -185,10 +250,10 @@ static void kmt_init(){
 	CPUS = _ncpu();
 	kmt->spin_init((spinlock_t *)&CT,"create tasks");
 	printf("%d\n",CPUS);
-	for(int i=0;i<8;i++) { osruntk[i] = 0;}
+	for(int i=0;i<8;i++) { osruntk[i] = 0; cpuisin[i] = -1;}
 	os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save);
 	os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
-	for(int i=0;i<20;i++) { tagging[i] = -1; tasks[i] = NULL; 
+	for(int i=0;i<20;i++) { tagging[i] = -1; tasks[i] = NULL; runable[i] = 0;
 		                mycpu[i].ncli = 0; mycpu[i].INIF = 1;
 	}
 
@@ -210,6 +275,7 @@ static int kmt_create(task_t *task, const char *name,
 	}
 	if(rec == -1) panic("too much pthreads\n");
 	tagging[rec] = rec;
+	runable[rec] = 1;
 	task->incpu = rec%CPUS;
 	task->tag = rec;
 	task->name = name;
@@ -261,6 +327,11 @@ static void kmt_spin_unlock(spinlock_t *lk){
 
 
 static void kmt_sem_init(sem_t *sem, const char *name, int value){
+	sem->name = name;
+	sem->count = value;
+	sem->lock = *(spinlock_t *)pmm->alloc(sizeof(spinlock_t));
+	kmt->spin_init((spinlock_t *)&sem->lock,"semlock");
+	for(int i=0;i<20;i++) sem->tks[i] = -1;
 
 }
 
@@ -268,14 +339,47 @@ static void kmt_sem_init(sem_t *sem, const char *name, int value){
 
 
 static void kmt_sem_wait(sem_t *sem){
-
+	kmt->spin_lock((spinlock_t *)&sem->lock);
+	sem->count--;
+	int label = 0;
+	if(sem->count < 0) {
+		label = 1;
+		int wtk = cpuisin[_cpu()];
+		for(int i=0;i<20;i++) {
+			if(sem->tks[i] != -1) {
+				if(wtk == i) panic("wrong!!!\n");
+				sem->tks[i]+=1;
+			}
+		}
+		sem->tks[wtk] = 0;
+		runable[wtk] = 0;
+		kmt->spin_unlock((spinlock_t *)&sem->lock);
+		_yield();
+	}
+	if(label == 0)
+	kmt->spin_unlock((spinlock_t *)&sem->lock);
 }
 
 
 
 
 static void kmt_sem_signal(sem_t *sem){
-
+	kmt->spin_lock((spinlock_t *)&sem->lock);
+	sem->count++;
+	if(sem->count < 0) {
+		int max = 0;
+		int rec = -1;
+		for(int i=0;i<20;i++) {
+			if(sem->tks[i] > max) {
+				max = sem->tks[i];
+				rec = i;
+			}
+		}
+		if(rec == -1) panic("wrong detected\n");
+		sem->tks[rec] = -1;
+		runable[rec] = 1;
+	}
+	kmt->spin_unlock((spinlock_t *)&sem->lock);
 }
 
 
