939c73e4e854504d2ea56bf1f96c60476588e3c7
tracer-nju <tracer@nju.edu.cn>
2019-06-07 20:04:48 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: c11fe16f759047f6a14199327200a2c9   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:04:48 up 10:02,  1 user,  load average: 0.18, 0.22, 0.25

--- src/vfs.c
+++ src/vfs.c
@@ -116,7 +116,7 @@ static int vfs_mkdir(filesystem_t *fs, const char *path,const char *name){
 		return -1;
 	}
 	printf("hello\n");
-	inode_t *now = fs->ops->lookup(fs,path,0,0);
+	inode_t *now = fs->ops->lookup(fs,"/abc",0,0);
 	printf("hello\n");
 	now->ops->mkdir(now, name);
 	return 0;
