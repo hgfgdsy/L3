593a12af46fc1bbe333863bb1fff5d95aa750495
tracer-nju <tracer@nju.edu.cn>
2019-06-07 17:41:41 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: c11fe16f759047f6a14199327200a2c9   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  17:41:42 up  7:39,  1 user,  load average: 0.35, 0.29, 0.22

--- src/fsops.c
+++ src/fsops.c
@@ -19,7 +19,7 @@ inode_t *f_lookup(struct filesystem *fs, const char *path, int flags, int from){
 		int I;
 		int dlen = fs->dev->ops->read(fs->dev, D, (void *)data, root.size);
 		inode_t *next;
-		next = (inode_t *)pmm->alloc(sizeof(inode));
+		next = (inode_t *)pmm->alloc(sizeof(inode_t));
 		for(int i = 1; i < len; i++){
 			if(*(path+i) != '/')
 				dir[lcnt++] = *(path+i);
@@ -46,7 +46,7 @@ inode_t *f_lookup(struct filesystem *fs, const char *path, int flags, int from){
 				}
 				if(label == 1){
 					fs->dev->ops->read(fs->dev,(MAP)+(I*64),(void *)next,sizeof(inode_t));
-					dlen = fs->dev->ops->read(fs->dev,(D)+((next.bid)*(1<<12)),(void *)data,next->size);
+					dlen = fs->dev->ops->read(fs->dev,(D)+((next->bid)*(1<<12)),(void *)data,next->size);
 					lcnt = 0;
 				}
 				else {
@@ -79,7 +79,7 @@ inode_t *f_lookup(struct filesystem *fs, const char *path, int flags, int from){
 			}
 			if(label == 1){
 				fs->dev->ops->read(fs->dev,(MAP)+I*64,(void *)next,sizeof(inode_t));
-				dlen = fs->dev->ops->read(fs->dev,(D)+(next.bid*(1<<12)),(void *)data,next->size);
+				dlen = fs->dev->ops->read(fs->dev,(D)+(next->bid*(1<<12)),(void *)data,next->size);
 				return next;
 			}
 			else{
