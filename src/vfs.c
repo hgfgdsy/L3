a3222a2f1cd23e7e85f46eef07e755902a4117c9
tracer-nju <tracer@nju.edu.cn>
2019-06-07 20:17:03 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: c11fe16f759047f6a14199327200a2c9   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:17:03 up 10:14,  1 user,  load average: 0.34, 0.32, 0.28

--- src/vfs.c
+++ src/vfs.c
@@ -54,9 +54,9 @@ static void vfs_init(){
 	vfs->mkdir(&EXT2, "/", "abc");
 	inode_t *temp = EXT2.ops->lookup(&EXT2,"/abc",0,0);
 	printf("%d\n",temp->bid);
-/*	vfs->mkdir(&EXT2, "/abc", "edf");
+	vfs->mkdir(&EXT2, "/abc", "edf");
 
-	inode_t *temp1 = EXT2.ops->lookup(&EXT2,"/abc/edf",0,0);
+/*	inode_t *temp1 = EXT2.ops->lookup(&EXT2,"/abc/edf",0,0);
 	inode_t *temp2 = EXT2.ops->lookup(&EXT2,"/abc/e",0,0);
 	if(temp2 == NULL) printf("!!!\n");
 	printf("%d\n",temp1->bid);*/
