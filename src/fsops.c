ab2deff1c759b3f86bdb78d16b8d945ecd8f0a70
tracer-nju <tracer@nju.edu.cn>
2019-06-08 10:24:06 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: f62718d81c084dafb8a24d4f0699100e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  10:24:06 up 19 min,  1 user,  load average: 0.34, 0.23, 0.35

--- src/fsops.c
+++ src/fsops.c
@@ -2,6 +2,61 @@
 #include <klib.h>
 #include <devices.h>
 
+
+int cref(inode_t *My, const char *name){
+	device_t *mi = (device_t *)My->ptr;
+	int nlen = strlen(name);
+	char omit[1];
+	int k;
+	for(k = 0; k < 960; k++){
+		mi->ops->read(mi,k,omit,1);
+		if(omit[0] == 0) break;
+	}
+	omit[0] = '1';
+	mi->ops->write(mi,k,omit,1);
+	char dname[nlen+1];
+	strncpy(dname,name,nlen);
+	dname[nlen] = '\0';
+	tory_t ap;
+	ap.I = k;
+	ap.rec_len = nlen + 9;
+	ap.file_type = 2;
+	ap.name_len = nlen;
+	mi->ops->write(mi, D + (My->bid)*(1<<12) + My->size,(void *)&ap,sizeof(ap));
+	mi->ops->write(mi, D + (My->bid)*(1<<12) + My->size + sizeof(ap),dname,nlen+1);
+	My -> size += ap.rec_len;
+	My->son++;
+	mi->ops->write(mi, MAP + (My->self)*64, (void *)My, sizeof(inode_t));
+	if(My->self == 0)
+		root.size = My->size;
+
+	inode_t new;
+	new.type = 1;
+	new.size = 11;
+	new.bid = k;
+	new.self = k;
+	new.ptr = (void *)mi;
+	new.fs = My->fs;
+	new.son = 0;
+	new.ops = My->ops;
+	mi->ops->write(mi, MAP + 64*k, (void *)&new, sizeof(inode_t));
+/*
+	tory_t ddot;
+	ddot.I = My->self;
+	ddot.rec_len = 11;
+	ddot.file_type = 1;
+	ddot.name_len = 2;
+	dname[0] ='.';
+	dname[1] ='.';
+	dname[2] ='\0';
+	mi->ops->write(mi, D + k*(1<<12),(void *)&ddot,sizeof(ddot));
+	mi->ops->write(mi, D + k*(1<<12)+ sizeof(ddot),dname,2+1);
+*/	return 0;
+}
+
+
+
+
 void f_init(struct filesystem *fs, const char *name, device_t *dev){
 	fs->dev = dev;
 	strcpy(fs->name,name);
@@ -89,8 +144,13 @@ inode_t *f_lookup(struct filesystem *fs, const char *path, int flags, int from){
 				return next;
 			}
 			else{
-				printf("Invalid path2!\n");
-				return NULL;
+				if(flags == 1) {
+					cref(next,dir);
+				}
+				else {
+				        printf("Invalid path2!\n");
+				        return NULL;
+				}
 			}
 		}
 	}
