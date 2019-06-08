e4b051c2fee7de45afaba827b2d249f2d5b61732
tracer-nju <tracer@nju.edu.cn>
2019-06-08 20:11:52 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: f62718d81c084dafb8a24d4f0699100e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:11:52 up 10:07,  1 user,  load average: 0.35, 0.86, 0.85

--- src/shell.c
+++ src/shell.c
@@ -18,8 +18,11 @@ void shell_thread(void *ttyid) {
 	char line[128];
 	int nread=0;
 	char path[128];
+	memset(path,0,sizeof(path));
 	path[0] = '/';
-	path[1] = '\0';
+	path[1] = 'a';
+	path[2] = 'b';
+	path[3] = 'c';
 	while(1){
 		if(nread != 0){
 			if(line[nread-1] == '\n'){
