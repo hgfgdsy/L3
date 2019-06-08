f4b64ee05e2fb8a587b15dde0e47354c6fa0d8eb
tracer-nju <tracer@nju.edu.cn>
2019-06-08 16:58:06 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: f62718d81c084dafb8a24d4f0699100e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  16:58:06 up  6:53,  1 user,  load average: 0.48, 0.31, 0.26

--- src/shell.c
+++ src/shell.c
@@ -1,21 +1,21 @@
 #include <common.h>
 #include <devices.h>
 #include <klib.h>
-
+extern ssize_t tty_write(device_t *dev, off_t offset, const void *buf, size_t count);
 void shell_thread(int tty_id) {
 	char buf[128];
 	strncpy(buf,"/dev/tty",8);
 	buf[8] = '0' + tty_id;
 	buf[9] = '\0';
 	int stdin = vfs->open(buf,O_RDONLY);
-	int stdout = vfs->open(buf,O_WRONLY);
+//	int stdout = vfs->open(buf,O_WRONLY);
 	char name[5];
 	strncpy(name,&buf[4],4);
 	name[4] = '\0';
 	device_t *tty = dev_lookup(name);
 	char text[1024];
 	char line[1024];
-	int nread;
+	int nread=0;
 	while(1){
 		if(nread!=0 && line[nread-1] == '\n'){
 			line[nread-1] = '\0';
@@ -24,7 +24,7 @@ void shell_thread(int tty_id) {
 		else{
 			sprintf(text,"(%s) $",name);
 			tty_write(tty, 0, text, strlen(name)+5);
-			nread = tty->ops->read(tty, 0, line, sizeof(line));
+			nread = tty->ops->read(stdin, 0, line, sizeof(line));
 		}
 	}
 }
