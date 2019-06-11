c1e31807b2a0df3d8b7dc2b5ac8505f21b8f4de5
tracer-nju <tracer@nju.edu.cn>
2019-06-11 21:21:43 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: 8f18a9110955458ba5d17ba879bc92b3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  21:21:43 up  1:06,  1 user,  load average: 0.26, 0.25, 0.31

--- src/kmt.c
+++ src/kmt.c
@@ -59,7 +59,7 @@ _Context *kmt_context_save(_Event ev, _Context *context) {
 	}
 	else{
 		current[_cpu()]->context = *context;
-		int tt = current[_cpu()]->tag;
+/*		int tt = current[_cpu()]->tag;
 		_io_read(_DEV_TIMER,_DEVREG_TIMER_UPTIME,&fclock,sizeof(fclock));
 		int ctm = fclock.lo - last_time;
 		last_time = fclock.lo;
@@ -79,7 +79,7 @@ _Context *kmt_context_save(_Event ev, _Context *context) {
 			T[tcnt-i-1] = temp[i];
 		}
 		strcpy(&pos[tt][len+8],T);
-
+*/
 	}
 //	        memcpy((void *)&(tasks[current[_cpu()] -> tag] -> context),(void *)context, sizeof(_Context));
 	return NULL;
